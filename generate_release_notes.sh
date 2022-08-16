1fa0dafe842f92c5a86035ff58868e653a9d0dda
James Mortensen <jmort253@gmail.com>
2022-08-16 20:16:21 +0530

    test [deploy] for beta images

--- generate_release_notes.sh
+++ generate_release_notes.sh
@@ -11,7 +11,7 @@ TAG_VERSION=${GRID_VERSION}-${BUILD_DATE}
 
 echo "" >> release_notes.md
 echo "### Changelog" > release_notes.md
-git --no-pager log "${LATEST_TAG}...${HEAD_BRANCH}" --pretty=format:"* [\`%h\`](http://github.com/seleniumhq/docker-${NAMESPACE}/commit/%H) - %s :: %an" --reverse >> release_notes.md
+git --no-pager log "${LATEST_TAG}...${HEAD_BRANCH}" --pretty=format:"* [\`%h\`](http://github.com/seleniumhq/docker-selenium/commit/%H) - %s :: %an" --reverse >> release_notes.md
 
 CHROME_VERSION=$(docker run --rm ${NAMESPACE}/node-chrome:${TAG_VERSION} google-chrome --version | awk '{print $3}')
 EDGE_VERSION=$(docker run --rm ${NAMESPACE}/node-edge:${TAG_VERSION} microsoft-edge --version | awk '{print $3}')
